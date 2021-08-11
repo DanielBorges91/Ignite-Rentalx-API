6d7eb195a915155c7e30e33a075869b8ae239cea
Daniel <danprog91@gmail.com>
2021-08-11 19:03:54 -0300

    Construindo autenticação nas rotas

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<Response | void> {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error("Token missing");
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "b0e2f3b8c6fc99532fcbb60dba8903a6"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists!");
+    }
+
+    next();
+  } catch (error) {
+    throw new Error("Invalid token!");
+  }
+}
